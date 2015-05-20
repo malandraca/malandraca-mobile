10ef22f848a2f91901b1be1619bbcb0ee99e295b
Gaston Aguilera <gaston.aguilera@globant.com>
2015-05-20 12:21:29 -0300

    Refactor

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,32 +1,73 @@
-"use strict";
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('malandracaApp', ['ionic'])
+// 'starter.controllers' is found in controllers.js
+angular.module('malandraca', ['ionic', 'malandraca.controllers'])
 
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/')
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  $stateProvider
-  .state('home', {
-    url: '/',
-    templateUrl: 'views/main.html'
-  })
-})
+        .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+    })
 
+    .state('app.news', {
+        url: "/news",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/news.html",
+                controller: 'NewsCtrl'
+            }
+        }
+    })
+
+    .state('app.radio', {
+            url: "/radio",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/radio.html",
+                    controller: 'RadioCtrl'
+                }
+            }
+        })
+        .state('app.config', {
+            url: "/config",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/config.html",
+                    controller: 'ConfigCtrl'
+                }
+            }
+        })
+
+    /*.state('app.single', {
+        url: "/playlists/:playlistId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/playlist.html",
+                controller: 'PlaylistCtrl'
+            }
+        }
+    });*/
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/radio');
+});
--- hooks/after_prepare/icons_and_splashscreens.js
+++ hooks/after_prepare/icons_and_splashscreens.js
@@ -20,7 +20,7 @@ var Orchestrator = require('orchestrator');
 
 var BASES = {
   android: 'res',
-  ios: 'MalandracaMobile/Resources'
+  ios: 'Malandraca/Resources'
 };
 var RESOURCE_DIR = 'resources';
 
